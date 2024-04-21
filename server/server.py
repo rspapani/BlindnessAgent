3d5e449339753155b39d712e9fe0276b52bf1c11
rspapani <rspapani@outlook.com>
2024-04-20 22:16:04 -0400

    :wq linked

--- server/server.py
+++ server/server.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import re
+
+from utils.ConvoCoach import ConvoCoach
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})  # Allow requests from any origin
 
-image_encodings = []
+machine = ConvoCoach()
 
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
@@ -12,10 +15,14 @@ def upload_image():
     if 'image_base64' in data:
         image_base64 = data['image_base64']
         timestamp = data['timestamp']
-        image_base64 = re.search(r'base64,(.*)', image_base64).group(1)
-        image_encodings.append(image_base64)
-        print(timestamp)
-        return jsonify({'message': 'Image uploaded successfully'}), 200
+        # image_base64 = re.search(r'base64,(.*)', image_base64).group(1)
+        # image_encodings.append(image_base64)
+        # print(timestamp)
+        # return jsonify({'message': 'Image uploaded successfully'}), 200
+
+        fers = machine.add_pic(image_base64, timestamp)
+        return jsonify({'message': f'Image analyzed {fers}'}), 200
+
     else:
         return jsonify({'error': 'No image_base64 field in the request body'}), 400
 
@@ -25,12 +32,18 @@ def upload_audio():
     data = request.get_json()
     if 'audio_url' in data:
         audio_url = data['audio_url']
+        print(audio_url)
         timestamp = data['timetamp']
         # Assuming the data format is a URL pointing to the audio file
         # You might want to perform additional validation on the URL
-        return jsonify({'audio_link': audio_url}), 200
+        # return jsonify({'audio_link': audio_url}), 200
+
+        feedback = machine.add_pic(audio_url, timestamp)
+        return jsonify({'message': f'Audio analyzed {feedback}'}), 200
     else:
         return jsonify({'error': 'No audio_url field in the request body'}), 400
+    
+
 @app.route('/dummy_endpoint', methods=['GET'])
 def dummy_endpoint():
     return jsonify({'message': 'This is a dummy endpoint'}), 200
